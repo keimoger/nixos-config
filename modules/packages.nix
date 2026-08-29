6358546019446421ca713a7a03029a62d1ed89e4
keimoger <keimoger@gmail.com>
2026-08-30 01:45:58 +0400

    Added autocomplete and moved nu's config to nix tracked locasyon

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,8 +1,28 @@
 { pkgs, ... }:
+let
+  # `atuin init nu` just prints a static script derived from the atuin
+  # binary itself, so generating it at build time keeps it pinned to
+  # whatever atuin version this nixpkgs revision carries.
+  atuinNuAutoload = pkgs.runCommand "atuin-nu-autoload" { } ''
+    export HOME=$TMPDIR
+    mkdir -p $out/share/nushell/vendor/autoload
+    ${pkgs.atuin}/bin/atuin init nu > $out/share/nushell/vendor/autoload/atuin.nu
+  '';
+
+  carapaceNuAutoload = pkgs.writeTextDir "share/nushell/vendor/autoload/carapace.nu" ''
+    $env.config.completions.external = {
+        enable: true
+        completer: {|spans| carapace $spans.0 nushell ...$spans | from json }
+    }
+  '';
+in
 {
   services.printing.enable = true;
 
-  programs.nushell.enable = true;
+  programs.nushell = {
+    enable = true;
+    autoloads = [ atuinNuAutoload carapaceNuAutoload ];
+  };
   programs.atuin.enable = true;
 
   environment.systemPackages = with pkgs; [
@@ -11,5 +31,6 @@
     helix
     nixd
     nixfmt
+    carapace
   ];
 }
