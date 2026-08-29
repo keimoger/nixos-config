ba0cec0efa90fe40f083f3f7a165bb8bc12b8e60
keimoger <keimoger@gmail.com>
2026-08-30 00:31:48 +0400

    Removed ghostty, added atuin

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,11 +3,11 @@
   services.printing.enable = true;
 
   programs.nushell.enable = true;
+  programs.atuin.enable = true;
 
   environment.systemPackages = with pkgs; [
     sbctl
     git
     helix
-    ghostty
   ];
 }
